9ef9f26f57b971597d5a71d49cb4e455f59a1fec
Paulo Vaz <53504601+PauloVaz-dev@users.noreply.github.com>
2022-04-11 23:22:44 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://paulovaz-dev-happy.herokuapp.com/'
+  baseURL: 'https://paulovaz-dev-happy.herokuapp.com'
 });
 
 export default api;
